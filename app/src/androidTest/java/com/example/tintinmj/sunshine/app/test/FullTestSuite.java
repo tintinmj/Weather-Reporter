bdd0bf1a4d3f9c0fa490e9432aaa1fe7cf5b78cc
tintinmj <anirban.nag9038541773@gmail.com>
2014-12-29 03:32:44 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/tintinmj/sunshine/app/test/FullTestSuite.java
@@ -0,0 +1,20 @@
+package com.example.tintinmj.sunshine.app.test;
+
+/**
+ * Created by tintinmj on 11-10-2014.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
